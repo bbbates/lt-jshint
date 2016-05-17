27f154cb32fb53b1f849bde0d4dc6d2bba653d62
Brendan Bates <batesbrendan@gmail.com>
2016-05-18 00:15:38 +1000

    Add readme, prepare for release

---
+++ README.md
@@ -0,0 +1,11 @@
+# lt-jshint - JSHint for Lighttable
+
+Integrates [jshint](http://jshint.com/) linting in Lighttable.
+
+
+
+## License
+
+Copyright © 2016 Brendan Bates
+
+Distributed under the Eclipse Public License, the same as Clojure.
--- src/lt/plugins/lt_jshint.cljs
+++ src/lt/plugins/lt_jshint.cljs
@@ -7,14 +7,13 @@
   (:require-macros [lt.macros :refer [behavior]]))
 
 (object/object* ::js-hinter
-                :triggers [:lt.objs.editor.lint/validate]
                 :behaviors [::do-jshint]
                 :linter-name "jshint"
                 :init (fn [this _ js-hint-opts]
                         (object/merge! this {:options js-hint-opts})))
 
 (behavior ::do-jshint
-          :triggers #{:lt.objs.editor.lint/validate}
+          :triggers #{:lt.plugins.lt-lint/validate}
           :reaction (fn [obj editor-text callback _]
                       (js/JSHINT editor-text (clj->js (:options @obj)) #js {})
                       (let [results (js->clj (.data js/JSHINT) :keywordize-keys true)
